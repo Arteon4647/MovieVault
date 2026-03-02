ada7a7c6b830838f2c82263ccc950def5d6d83f2
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-02 14:15:20 +0200

    refactor, create ScreenSizesPreview file

--- app/src/main/java/com/example/movievault/presentation/components/MovieButton.kt
+++ app/src/main/java/com/example/movievault/presentation/components/MovieButton.kt
@@ -64,52 +64,6 @@ fun SecondaryMovieButton(
     }
 }
 
-@Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.FUNCTION)
-@Preview(
-    group = "Screen Size",
-    name = "Small Light",
-    device = "id:Galaxy Nexus",
-    showSystemUi = false,
-    uiMode = Configuration.UI_MODE_NIGHT_NO
-)
-@Preview(
-    group = "Screen Size",
-    name = "Small Dark",
-    device = "id:Galaxy Nexus",
-    showSystemUi = false,
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-    group = "Screen Size",
-    name = "Medium Light",
-    device = "id:pixel_2",
-    showSystemUi = false,
-    uiMode = Configuration.UI_MODE_NIGHT_NO
-)
-@Preview(
-    group = "Screen Size",
-    name = "Medium Dark",
-    device = "id:pixel_2",
-    showSystemUi = false,
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-    group = "Screen Size",
-    name = "Large Light",
-    device = "id:pixel_8",
-    showSystemUi = false,
-    uiMode = Configuration.UI_MODE_NIGHT_NO
-)
-@Preview(
-    group = "Screen Size",
-    name = "Large Dark",
-    device = "id:pixel_8",
-    showSystemUi = false,
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-annotation class ScreenSizesPreview
-
 @ScreenSizesPreview
 @Composable
 fun MovieButtonsPreview() {
