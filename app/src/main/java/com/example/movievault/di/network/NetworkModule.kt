9af6ed4919068dd9ce6d7910b06fad33b37c2368
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-13 15:21:49 +0200

    add dependency injection on Hilt

--- app/src/main/java/com/example/movievault/di/network/NetworkModule.kt
+++ app/src/main/java/com/example/movievault/di/network/NetworkModule.kt
@@ -1,11 +1,44 @@
 package com.example.movievault.di.network
 
+import com.example.movievault.data.remote.api.ApiKeyInterceptor
+import com.example.movievault.data.remote.api.MovieVaultApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(apiKeyInterceptor: ApiKeyInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(apiKeyInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieVaultApiService(retrofit: Retrofit): MovieVaultApiService {
+        return retrofit.create(MovieVaultApiService::class.java)
+    }
 
 }
