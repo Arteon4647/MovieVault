6182f1c90ddd490af8ae2bd45bcd6e308cf27995
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-13 15:11:02 +0200

    add timeout in Retrofit

--- app/src/main/java/com/example/movievault/data/remote/api/Retrofit.kt
+++ app/src/main/java/com/example/movievault/data/remote/api/Retrofit.kt
@@ -3,9 +3,13 @@ package com.example.movievault.data.remote.api
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val client = OkHttpClient.Builder()
     .addInterceptor(ApiKeyInterceptor())
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .readTimeout(30, TimeUnit.SECONDS)
+    .writeTimeout(30, TimeUnit.SECONDS)
     .build()
 
 val retrofit = Retrofit.Builder()
