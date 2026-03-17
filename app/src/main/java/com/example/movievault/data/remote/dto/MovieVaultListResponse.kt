e9dfd045770ad48f649458a279f7485d56d9bd94
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-17 10:35:41 +0200

    add Serialized name for all fields in MovieVaultListResponse

--- app/src/main/java/com/example/movievault/data/remote/dto/MovieVaultListResponse.kt
+++ app/src/main/java/com/example/movievault/data/remote/dto/MovieVaultListResponse.kt
@@ -3,7 +3,9 @@ package com.example.movievault.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class MovieVaultListResponse(
+    @SerializedName("page")
     val page: Int,
+    @SerializedName("results")
     val results: List<MovieVaultDto>,
     @SerializedName("total_pages")
     val totalPages: Int,
