5fafae415da2e89cc8d48aef516fe3abb4df3aa0
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-17 10:34:35 +0200

    add Serialized name for all fields in MovieVaultDetailResponse

--- app/src/main/java/com/example/movievault/data/remote/dto/MovieVaultDetailResponse.kt
+++ app/src/main/java/com/example/movievault/data/remote/dto/MovieVaultDetailResponse.kt
@@ -3,8 +3,11 @@ package com.example.movievault.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class MovieVaultDetailResponse(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("overview")
     val overview: String?,
     @SerializedName("poster_path")
     val posterPath: String?,
@@ -14,10 +17,13 @@ data class MovieVaultDetailResponse(
     val releaseDate: String?,
     @SerializedName("vote_average")
     val voteAverage: Float,
+    @SerializedName("genres")
     val genres: List<GenreDto>
 )
 
 data class GenreDto(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("name")
     val name: String
 )
