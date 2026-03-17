f1073f8cca61d185cfb3f21dd484694a9d463c5d
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-17 09:50:47 +0200

    and add check null in use case

--- app/src/main/java/com/example/movievault/domain/usecase/GetMovieDetailsUseCase.kt
+++ app/src/main/java/com/example/movievault/domain/usecase/GetMovieDetailsUseCase.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 class GetMovieDetailsUseCase @Inject constructor(
     private val repository: MovieRepository
 ) {
-    suspend operator fun invoke(movieId: Int): MovieDetails {
+    suspend operator fun invoke(movieId: Int): MovieDetails? {
         return repository.getMovieDetails(movieId)
     }
 }
