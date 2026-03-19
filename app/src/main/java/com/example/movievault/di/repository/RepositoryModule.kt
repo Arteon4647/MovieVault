392543fdbc96eb712db70a50503c7694cc48e831
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-19 09:36:37 +0200

    add RepositoryModule by di

--- app/src/main/java/com/example/movievault/di/repository/RepositoryModule.kt
+++ app/src/main/java/com/example/movievault/di/repository/RepositoryModule.kt
@@ -1,11 +1,17 @@
 package com.example.movievault.di.repository
 
+import com.example.movievault.data.repository.MovieRepositoryImpl
+import com.example.movievault.domain.repository.MovieRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindMovieRepository(
+        impl: MovieRepositoryImpl
+    ): MovieRepository
 }
