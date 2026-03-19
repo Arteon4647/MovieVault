eb70819e84b4aa95928900660cd83227a4af4c77
Arteon4647 <artemflashsharapov@gmail.com>
2026-03-19 09:49:25 +0200

    add DatabaseModule by di

--- app/src/main/java/com/example/movievault/di/db/DatabaseModule.kt
+++ app/src/main/java/com/example/movievault/di/db/DatabaseModule.kt
@@ -1,11 +1,34 @@
 package com.example.movievault.di.db
 
+import android.content.Context
+import androidx.room.Room
+import com.example.movievault.data.local.db.FavoriteMovieDao
+import com.example.movievault.data.local.db.MovieVaultAppDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+const val DATABASE_NAME = "movievault_db"
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): MovieVaultAppDatabase {
+        return Room.databaseBuilder(
+            context,
+            MovieVaultAppDatabase::class.java,
+            DATABASE_NAME
+        ).fallbackToDestructiveMigration(false).build()
+    }
 
+    @Provides
+    @Singleton
+    fun provideFavoriteMovieDao(database: MovieVaultAppDatabase): FavoriteMovieDao {
+        return database.favoriteMovieDao()
+    }
 }
