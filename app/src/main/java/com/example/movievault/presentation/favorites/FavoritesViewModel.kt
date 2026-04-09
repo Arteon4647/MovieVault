3f3ba3e42760989c0c5c309ee46a17374165eb3a
Arteon4647 <artemflashsharapov@gmail.com>
2026-04-09 16:01:27 +0300

    add FavoritesViewModel

--- app/src/main/java/com/example/movievault/presentation/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/movievault/presentation/favorites/FavoritesViewModel.kt
@@ -1,2 +1,31 @@
 package com.example.movievault.presentation.favorites
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movievault.domain.model.Movie
+import com.example.movievault.domain.usecase.GetFavoriteMoviesUseCase
+import com.example.movievault.domain.usecase.ToggleFavoriteUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    getFavoriteMoviesUseCase: GetFavoriteMoviesUseCase,
+    private val toggleFavoriteUseCase: ToggleFavoriteUseCase
+) : ViewModel() {
+    val favorites = getFavoriteMoviesUseCase()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    fun onFavoriteClick(movie: Movie) {
+        viewModelScope.launch {
+            toggleFavoriteUseCase(movie)
+        }
+    }
+}
