b3b8203f4bb3b0063dafd8e709e790aaad0d851b
Arteon4647 <artemflashsharapov@gmail.com>
2026-04-09 17:28:34 +0300

    add FavoritesScreen and EmptyState

---
+++ app/src/main/java/com/example/movievault/presentation/favorites/FavoritesScreen.kt
@@ -0,0 +1,43 @@
+package com.example.movievault.presentation.favorites
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.movievault.presentation.components.MovieCard
+
+@Composable
+fun FavoritesScreen(
+    viewModel: FavoritesViewModel = hiltViewModel(),
+    onMovieClick: (Int) -> Unit
+) {
+    val movies by viewModel.favorites.collectAsStateWithLifecycle()
+
+    if (movies.isEmpty()) {
+        EmptyState()
+    } else {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(movies) { movie ->
+                MovieCard(
+                    movie = movie,
+                    onClick = { onMovieClick(movie.id) },
+                    isFavorite = true,
+                    onFavoriteClick = {
+                        viewModel.onFavoriteClick(movie)
+                    }
+                )
+            }
+        }
+    }
+}
