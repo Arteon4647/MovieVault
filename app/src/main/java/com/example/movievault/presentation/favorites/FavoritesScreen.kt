8f318df3fef6de27c79a78905fdd16fac727a54d
Arteon4647 <artemflashsharapov@gmail.com>
2026-04-10 18:28:56 +0300

    add feedback to errors

--- app/src/main/java/com/example/movievault/presentation/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/example/movievault/presentation/favorites/FavoritesScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -18,6 +21,14 @@ fun FavoritesScreen(
     onMovieClick: (Int) -> Unit
 ) {
     val movies by viewModel.favorites.collectAsStateWithLifecycle()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+
+    LaunchedEffect(Unit) {
+        viewModel.errors.collect { message ->
+            snackbarHostState.showSnackbar(message)
+        }
+    }
 
     if (movies.isEmpty()) {
         EmptyState()
