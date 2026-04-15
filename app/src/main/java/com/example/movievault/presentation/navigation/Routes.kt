62015f572e112a8d5b74424f5d936782188861b8
Arteon4647 <artemflashsharapov@gmail.com>
2026-04-15 14:01:05 +0300

    add Routes

--- app/src/main/java/com/example/movievault/presentation/navigation/Routes.kt
+++ app/src/main/java/com/example/movievault/presentation/navigation/Routes.kt
@@ -1,2 +1,12 @@
 package com.example.movievault.presentation.navigation
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+object HomeRoute
+
+@Serializable
+object FavoriteRoute
+
+@Serializable
+data class DetailsRoute(val movieId: Int)
