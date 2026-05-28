138d65a2482c8437994be4156f6021906ada1943
Arteon4647 <artemflashsharapov@gmail.com>
2026-05-28 16:29:05 +0300

    add Crashlytics tree

--- app/src/main/java/com/example/movievault/MovieVaultApp.kt
+++ app/src/main/java/com/example/movievault/MovieVaultApp.kt
@@ -1,7 +1,19 @@
 package com.example.movievault
 
 import android.app.Application
+import com.example.movievault.logging.CrashlyticsTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MovieVaultApp : Application()
+class MovieVaultApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(CrashlyticsTree())
+        }
+    }
+}
--- app/src/main/java/com/example/movievault/presentation/components/NetworkError.kt
+++ app/src/main/java/com/example/movievault/presentation/components/NetworkError.kt
@@ -1,5 +1,6 @@
 package com.example.movievault.presentation.components
 
+import timber.log.Timber
 import java.io.IOException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
@@ -9,4 +10,8 @@ fun Throwable.toUserMessage(): String = when (this) {
     is SocketTimeoutException -> "Connection timed out. Try again"
     is IOException -> "Network error. Check your connection"
     else -> message ?: "Something went wrong"
+}
+
+fun Throwable.logError(tag: String = "MovieVault", message: String = "Error occurred") {
+    Timber.tag(tag).e(this, message)
 }
