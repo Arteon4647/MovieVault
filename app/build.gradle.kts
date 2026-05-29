a29ea23a29142385f85cfc8c10fe7c2adbec0719
Arteon4647 <artemflashsharapov@gmail.com>
2026-05-29 09:47:05 +0300

    add implementation messaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,6 +110,7 @@ dependencies {
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.analytics)
     implementation(libs.timber)
+    implementation(libs.firebase.messaging)
 }
 
 ksp {
